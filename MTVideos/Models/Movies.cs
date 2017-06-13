3eef4dea9e85b3ee3366bba5995d5814c30c1945
Matthew Tynan <Tynan45@hotmail.com>
2017-06-13 21:31:39 +0100

    Added Movies and Genre models

---
+++ MTVideos/Models/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MTVideos.Models
+{
+    public class Movies
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateReleased { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
+
+        public Genres Genre { get; set; }
+        public byte GenreId { get; set; }
+    }
+}
