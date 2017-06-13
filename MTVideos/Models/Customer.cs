615005f0d3b63ff6a58ff6ce23a384b6220c659c
Matthew Tynan <Tynan45@hotmail.com>
2017-06-13 19:29:47 +0100

    Added Customer and MembershipType models

---
+++ MTVideos/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MTVideos.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public bool IsSubscribed { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
+
+    }
+}
