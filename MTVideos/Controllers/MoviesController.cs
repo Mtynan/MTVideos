cdba16031a03c97c1b0e87a1dca859263e1ca290
Matt <Mtynan4@gmail.com>
2017-06-15 17:32:43 +0100

    Edit/Add new Movie to the database

--- MTVideos/Controllers/MoviesController.cs
+++ MTVideos/Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Data.Entity;
 using MTVideos.Models;
 using MTVideos.ViewModels;
+using System.Data.Entity.Validation;
 
 namespace MTVideos.Controllers
 {
@@ -44,6 +45,47 @@ namespace MTVideos.Controllers
             var viewModel = new MovieFormViewModel
             {
                 Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movies movies)
+        {       
+            if (movies.Id == 0)
+            {
+                movies.DateAdded = DateTime.Now;
+                _context.Movies.Add(movies);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movies.Id);
+                movieInDb.Name = movies.Name;
+                movieInDb.NumberInStock = movies.NumberInStock;
+                movieInDb.GenreId = movies.GenreId;
+                movieInDb.DateReleased = movies.DateReleased;
+
+            }
+           
+            
+            _context.SaveChanges();
+            
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if  (movie == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new MovieFormViewModel
+            {
+                Movies = movie,
+                Genres = _context.Genres.ToList()
             };
 
             return View("MovieForm", viewModel);
---
+++ MTVideos/Migrations/201706142235434_testnulldatetimeinmovies.cs
@@ -0,0 +1,20 @@
+namespace MTVideos.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class testnulldatetimeinmovies : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "DateReleased", c => c.DateTime());
+            AlterColumn("dbo.Movies", "DateAdded", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "DateAdded", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Movies", "DateReleased", c => c.DateTime(nullable: false));
+        }
+    }
+}
