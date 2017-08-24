451748957965f465e03b691242e2785fcfb0429e
Matt <Mtynan4@gmail.com>
2017-08-24 15:24:11 +0100

    Updated Mapping profiles and added MoviesDto

--- MTVideos/App_Start/MappingProfile.cs
+++ MTVideos/App_Start/MappingProfile.cs
@@ -12,8 +12,16 @@ namespace MTVideos.App_Start
     {
         public MappingProfile()
         {
+            // Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movies, MoviesDto>();
+
+            //Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+            
+            Mapper.CreateMap<MoviesDto, Movies>()
+                           .ForMember(c => c.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ MTVideos/Dtos/MoviesDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MTVideos.Dtos
+{
+    public class MoviesDto
+    {
+ 
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    
+        [Required]
+        public DateTime DateReleased { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
