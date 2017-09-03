f5f854fb2e8b664e2f48decbed85334bf2d13ae5
Matt <Mtynan4@gmail.com>
2017-09-03 22:40:51 +0100

    Prep for Rental api

---
+++ MTVideos/Controllers/Api/RentalController.cs
@@ -0,0 +1,21 @@
+﻿using MTVideos.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MTVideos.Controllers.Api
+{
+    public class RentalController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals (NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ MTVideos/Dtos/NewRentalDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MTVideos.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+    }
+}
