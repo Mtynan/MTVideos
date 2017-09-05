23fba4cc575191f94d0b934e4dc28fbed084eafc
Matt <Mtynan4@gmail.com>
2017-09-05 17:17:33 +0100

    added Rental Form

--- MTVideos/Controllers/Api/RentalController.cs
+++ MTVideos/Controllers/Api/RentalController.cs
@@ -13,6 +13,11 @@ namespace MTVideos.Controllers.Api
     {
         private ApplicationDbContext _context;
 
+        public RentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals (NewRentalDto newRental)
         {
@@ -20,10 +25,13 @@ namespace MTVideos.Controllers.Api
                 c => c.Id == newRental.CustomerId);
 
             var movies = _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental
@@ -35,6 +43,7 @@ namespace MTVideos.Controllers.Api
 
                 _context.Rentals.Add(rental);
             }
+
 
             _context.SaveChanges();
 
